69acc26b5f4304a17e8a5aaf167fc3406dd86420
Robin Schmitz <schmitz.robin@gmx.de>
2024-10-05 15:24:32 +0200

    Confines Following Camera - There is no turning back

--- ld56/Assets/ConfineFollow.cs
+++ ld56/Assets/ConfineFollow.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DumbCamera : MonoBehaviour
+public class ConfineFollow : MonoBehaviour
 {
     [SerializeField] private GameObject toTrack;
     private float maxX;
+    private float prevPosX;
     
     // Start is called before the first frame update
     void Start()
@@ -22,5 +23,8 @@ public class DumbCamera : MonoBehaviour
             this.transform.position += Vector3.right * (trackX - maxX);
             maxX = trackX;
         }
+        if(transform.position.x < prevPosX) transform.position = new Vector3(prevPosX, transform.position.y, transform.position.z);
+        
+        prevPosX = transform.position.x;
     }
 }
