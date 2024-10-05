3459c8fd6f49ee739f779ad219bf1e77ae9df194
Robin Schmitz <schmitz.robin@gmx.de>
2024-10-05 15:59:38 +0200

    Waterdrop

---
+++ ld56/Assets/WaterDrop.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDrop : MonoBehaviour
+{
+    [SerializeField] private float startSize = 0.5f;
+    [SerializeField] private float stepSize = 0.5f;
+    
+    [SerializeField] private float stepTime = 1f;
+    [SerializeField] private int stepsUntilDrop = 3;
+
+    private float timer = 0f;
+    private int stepCount = 0;
+
+    private Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        transform.localScale = new Vector3(startSize, startSize, startSize);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(stepCount >= stepsUntilDrop) {
+            rigidbody.bodyType = RigidbodyType2D.Dynamic;
+        } else {
+            if(timer >= stepTime) {
+                stepCount++;
+                transform.localScale = new Vector3(transform.localScale.x + stepSize, transform.localScale.y + stepSize, transform.localScale.z + stepSize);
+                timer = 0;
+            }
+        }
+    }
+}
